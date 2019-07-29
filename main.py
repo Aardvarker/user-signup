76ac5f21f70d1743b66b3688764c00aa41cd083a
Aardvarker <TheLuTz@GMail.com>
2019-07-29 00:01:10 -0500

    Final

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
-#!/usr/bin/env python
-from flask import Flask, request
-import os
-import jinja2
-
-templates_dr = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_dr))
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-
 @app.route("/")
 def index():
-    template = jinja_env.get_template('user_signup_template.html')
-    return template.render()
+    return render_template('index.html')
 
-if __name__=='__main__':
-    app.run()
+@app.route("/", methods=['POST'])
+def validate():
+    username = request.form['username']
+    pw = request.form['pw']
+    pver = request.form['pver']
+    email = request.form['email']
+
+    username_error = ''
+    pw_error = ''  
+    pver_error = ''
+    email_error = ''
+
+    if username == '':
+        username_error = 'Please enter a valid username'
+    elif len(username) < 3 or len(username) > 20 or " " in username:
+        username_error = 'Username must contain between 3 and 20 characters and cannot contain spaces.'
+        
+    if pw == '':
+        pw_error = 'Please enter a valid password'
+    elif len(pw) < 3 or len(pw) > 20 or " " in pw:
+        pw_error = 'Password must contain between 3 and 20 characters and cannot contain spaces.'
+
+    if pw != pver:
+        pver_error = 'Password does not match.'
+        
+    if email != '':
+        if len(email) < 3 or len(email) > 20 or " " in email or "@" not in email or "." not in email:
+            email_error = "Please enter a valid email address."
+
+    if not username_error and not pw_error and not pver_error and not email_error:
+        return redirect('/welcome?username={}'.format(username))
+    else:
+        return render_template('index.html', username_error = username_error, pw_error = pw_error, 
+            pver_error = pver_error, email_error = email_error, username = username, email = email)
+
+@app.route("/welcome")
+def welcome():
+    username = request.args.get('username')
+    return render_template('welcome.html', username = username)
+    
+app.run()
+
